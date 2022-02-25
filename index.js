58d98ae0c7e6fd1622cbcd62ef281596d1329c1f
Vinicius Yonezawa <vincius.yonezawa@gmail.com>
2022-02-24 22:42:28 -0300

    Styling with bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get("/", (req,res) => { 
-    res.send("Creating the website!");
+
+    var name = req.params.name;
+    res.render("index", {
+        name : name,
+
+    });
+});
+
+app.get("/question", (req,res) => { 
+
+    var name = req.params.name;
+    res.render("questions", {
+        name : name,
+
+    });
 });
 
 app.listen(2201, () => { console.log("Listening on port 2201");});
